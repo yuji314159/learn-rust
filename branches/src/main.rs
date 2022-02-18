ee0f22719f5c680dfef23099b5a05086aa0aace2
yuji314159 <yuji314159@gmail.com>
2022-02-18 23:49:04 +0900

    フロー制御

---
+++ branches/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let x = 5;
+
+    if x < 5 {
+        println!("smaller than five");
+    } else if x > 5 {
+        println!("bigger than five");
+    } else {
+        println!("FIVE!");
+    }
+}
