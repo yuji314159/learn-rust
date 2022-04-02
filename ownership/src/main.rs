bc4d496bfa3e6486136d080a70b4b22d1d43dbbf
yuji314159 <yuji314159@gmail.com>
2022-04-02 23:34:00 +0900

    f

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -7,4 +7,18 @@ fn main() {
     let mut d = c;
     d[0] = 0;
     println!("{} {}", c[0], d[0]);
+
+    let mut s = String::from("hello");
+    let t = &s;
+    f1(&s);
+    // f2(&mut s);
+    println!("{} {}", s, t);
+}
+
+fn f1(s: &String) {
+    println!("{}", s);
+}
+
+fn f2(s: &mut String) {
+    println!("{}", s);
 }
